b055ba941f27355f40a8ddb5d2bfe8af50c7df35
lokesh336 <rlokesh219@gmail.com>
2024-02-28 18:53:56 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import './App.css';
+import Head from './components/Header';
+import Home from './components/home';
+import Card from './components/card';
+import Footer from './components/footer';
+import About from './components/about';
+import Service from './components/service';
+import Client from './components/client';
+import Booking from './components/booking';
+import Deliver from './components/deliver';
+import Head1 from './components/header1';
+import Price from './components/price';
+import Transport from './components/transport';
+import Blog from './components/blog';
+import Partner from './components/partner';
+// import Slider from './components/slider';
+
+function App() {
+  return (
+    <>
+    <Head1/>
+    <Head/>
+    <Home/>
+    <Card/>
+    <About/>
+    <Service/>
+    <Client/>
+    <Booking/>
+    <Deliver/>
+    <Price/>
+    <Transport/>
+    <Blog/>
+    <Partner/>
+    <Footer/>
+    
+    </>
+  );
+}
+
+export default App;
